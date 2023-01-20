464ec6d48b0a99039481c78818f2b6879f0179a2
leidyp6 <leidy.johannap@hotmail.com>
2023-01-19 22:13:10 -0500

    Se graba función validar teclas de letras

--- Operacion/cerebro.js
+++ Operacion/cerebro.js
@@ -1,33 +1,76 @@
-//import [dibujarCanvas]from'./canvas.js';
-
 // VARIABLES
 let conjuntoPalabras =["caminar", "nadar", "volar", "correr", "saltar", "bailar", "girar", "patinar", "ejercicio"];
 //Capturar canvas
 let tablero = document.getElementById("horca").getContext("2d");
 //Palabra para adivinar
 var palabraSecreta = "";
-//Elemento html donde se escribirá la palabra
-let respuesta = document.getElementsByClassName("adivinaEsto");
-//Variable que pinta lineas de palabra secreta
+//Array para almacenar letras
+let letraIn = [];
+let letraErronea = [];
 
+function ocultarElementos(){
+	
+	document.getElementById("espaciodeJuego").style.display = "none";
+	document.getElementById("horca").style.display = "none";	
+}
 
+//ocultarElementos();
 
 //Elegir palabra aleatoria
 function elegirPalabraSecreta(){
 	var sortearPalabra = conjuntoPalabras[Math.floor(Math.random()* conjuntoPalabras.length)];
 	palabraSecreta = sortearPalabra.toUpperCase();
 	console.log(palabraSecreta);
+}
+
+function campoParaAdivinar(){
 	const guionesPalabraSecreta = palabraSecreta.replace(/./g, " _ ");
-	document.getElementById("campoGuiones").innerHTML=guionesPalabraSecreta;
-	//console.log(guionesPalabraSecreta);
-	return guionesPalabraSecreta;
+	let objetoConGuiones = document.getElementById("campoGuiones").innerHTML = guionesPalabraSecreta;
+	console.log(guionesPalabraSecreta);
+	//return guionesPalabraSecreta;
+}
+
+function esLetra(key){
+	let estado = false;
+	if (key >= 65 && letraIn.indexOf(key) || key <= 90 && letraIn.indexOf(key)){
+		letraIn.push(key);
+		console.log(key, estado);
+		return estado;
+	} else {
+		estado = true;
+		console.log(key, estado);
+		return estado;
+	}
+}
+/*
+function capturaLetraMala(){
+	let objetoConGuiones = document.getElementById("guionesLetrasMal").innerHTML = letraErronea;
+}*/
+
+function evaluarLetras(){
+		
+	for(i = 0; i < palabraSecreta.length; i++){
+		if(palabraSecreta.includes(validaLetra)){
+			console.log("FUNCIONA");
+			let cambiaGuiones
+		} else {
+			console.log("no es letra");
+		}
+	}
 	
 }
 
 function iniciarJuego() {
 	document.getElementById("comenzar-juego").style.display = "none";
 	elegirPalabraSecreta();
+	campoParaAdivinar();
 
 	dibujarCanvas();
+	etiquetaLetras();
 	dibujarGuiones();
+
+	document.onkeydown = (e) => {
+		let letra = e.key.toUpperCase();
+		esLetra(letra);
+	}
 }
