2106199ada57bb2ce581f7374b7fa772642bc79f
leidyp6 <leidy.johannap@hotmail.com>
2023-01-31 15:19:34 -0500

    Mostrar arreglo de letras incorrectas funcionando

--- Operacion/cerebro.js
+++ Operacion/cerebro.js
@@ -1,38 +1,33 @@
 // VARIABLES
-let conjuntoPalabras =["caminar", "nadar", "volar", "correr", "saltar", "bailar", "girar", "patinar", "ejercicio"];
+let conjuntoPalabras = ["caminar", "nadar", "volar", "correr", "saltar", "bailar", "girar", "patinar", "ejercicio"];
 //Capturar canvas
 let tablero = document.getElementById("horca").getContext("2d");
+// HTML donde se pintan letras incorrectas
+let conjuntoLetrasMal = [];
 //Palabra para adivinar
 var palabraSecreta = "";
 //Array para almacenar letras
+let guionesPalabraSecreta;
 let letraIn = [];
-let letraErronea = [];
+let intentos = 8;
+let listaLetrasMal = document.getElementById("letraMal");
 
-function ocultarElementos(){
-	
-	document.getElementById("espaciodeJuego").style.display = "none";
-	document.getElementById("horca").style.display = "none";	
+function ocultarElementos() {
+	document.getElementById("adivinaContainer").style.display = "none";
 }
 
-//ocultarElementos();
+ocultarElementos();
 
 //Elegir palabra aleatoria
-function elegirPalabraSecreta(){
-	var sortearPalabra = conjuntoPalabras[Math.floor(Math.random()* conjuntoPalabras.length)];
+function elegirPalabraSecreta() {
+	var sortearPalabra = conjuntoPalabras[Math.floor(Math.random() * conjuntoPalabras.length)];
 	palabraSecreta = sortearPalabra.toUpperCase();
 	console.log(palabraSecreta);
 }
 
-function campoParaAdivinar(){
-	const guionesPalabraSecreta = palabraSecreta.replace(/./g, " _ ");
-	let objetoConGuiones = document.getElementById("campoGuiones").innerHTML = guionesPalabraSecreta;
-	console.log(guionesPalabraSecreta);
-	//return guionesPalabraSecreta;
-}
-
-function esLetra(key){
+function comprobarSiEsLetra(key) {
 	let estado = false;
-	if (key >= 65 && letraIn.indexOf(key) || key <= 90 && letraIn.indexOf(key)){
+	if (key >= 65 && letraIn.indexOf(key) || key <= 90 && letraIn.indexOf(key)) {
 		letraIn.push(key);
 		console.log(key, estado);
 		return estado;
@@ -42,35 +37,36 @@ function esLetra(key){
 		return estado;
 	}
 }
-/*
-function capturaLetraMala(){
-	let objetoConGuiones = document.getElementById("guionesLetrasMal").innerHTML = letraErronea;
-}*/
-
-function evaluarLetras(){
-		
-	for(i = 0; i < palabraSecreta.length; i++){
-		if(palabraSecreta.includes(validaLetra)){
-			console.log("FUNCIONA");
-			let cambiaGuiones
-		} else {
-			console.log("no es letra");
-		}
-	}
-	
-}
 
 function iniciarJuego() {
-	document.getElementById("comenzar-juego").style.display = "none";
+	document.getElementById("comenzar-juego").style.display = "flex"; //"none";
+	document.getElementById("adivinaContainer").style.display = "flex";
+	document.getElementById("horca").style.display = "flex";
 	elegirPalabraSecreta();
-	campoParaAdivinar();
-
 	dibujarCanvas();
 	etiquetaLetras();
 	dibujarGuiones();
 
 	document.onkeydown = (e) => {
 		let letra = e.key.toUpperCase();
-		esLetra(letra);
-	}
+		if (comprobarSiEsLetra(letra) && palabraSecreta.includes(letra)) {
+			for (let i = 0; i < palabraSecreta.length; i++) {
+				if (palabraSecreta[i] === letra) {
+					agregaLetrasOk(i);
+				}
+			}
+		} else {
+			intentos--;
+			if (intentos > 0) {
+				document.getElementById("conteoIntentos").innerHTML = "Intenta de nuevo, te quedan " + intentos + " intentos";		
+				conjuntoLetrasMal.push(letra);
+				listaLetrasMal.innerHTML = `<p>${conjuntoLetrasMal}</p>`;
+				} else if (intentos <= 0) {
+					document.getElementById("conteoIntentos").innerHTML = "FIN DEL JUEGO";
+					
+					}
+		
+				}
+		}
+
 }
